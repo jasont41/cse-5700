f3ec2e8757b79b11c10698eb8abab93257d13b8d
Jason Eastman <jasont41@gmail.com>
2021-11-06 21:00:47 -0700

    identifier parsing working
    Need to implement: functions, classes, structs, enums, and variable declarations that have multiple declarations in one line. I.e " int num1, num2, num3...

--- Lab2/Lex.cpp
+++ Lab2/Lex.cpp
@@ -4,8 +4,9 @@
 #include <string> 
 using namespace std; 
 
-void vector_print(vector<string>); 
+void print_vector(vector<string>); 
 int check_line(vector<string>&, vector<int>&, string, int); 
+string get_identifier(string); 
 int main()
 {
     string line; 
@@ -25,10 +26,13 @@ int main()
     }
     //vector_print(lines); 
     for(int i = 0; i < lines.size(); i++){
-        check_line(identifier, location, lines[i], i); 
+        //  Return 1 == good, return 8 == ignored main
+        int return_code = check_line(identifier, location, lines[i], i); 
+        //  Uncomment for debugging; 
+        //cout << return_code << endl; 
     }
-
     
+    print_vector(identifier); 
 }
 
 
@@ -40,16 +44,35 @@ int check_line(vector<string>& ident, vector<int>& loc, string line, int line_nu
     string sub[7] =  {"char", "short", "long", "int", "float","double","bool"}; 
     for(int i = 0; i < 7; i++)
     {
-        if(line.find(sub[i]) != string::npos){
-            cout << "found" << endl << i << endl; 
-            return 1;  
+        // finds identifier, then isolates declaration  
+        if(line.find(sub[i]) != string::npos){  
+
+            int index = line.find(sub[i]); 
+            string identif = get_identifier(line.substr(sub[i].length()+index, line.length())); 
+
+            //  Ignore main if there as it wasn't user created and thus isn't a identifier 
+            if(identif.find("main") != string::npos)
+            {
+                return 8;  
+            }
+            else
+            {
+                ident.push_back(get_identifier(line.substr(sub[i].length()+index, line.length())));
+                loc.push_back(line_num); 
+                return 1; 
+            }
         }
     }
-
-
+    return 0; 
 }
 
-void vector_print(vector<string> vect)
+
+
+string get_identifier(string input)
+{
+    return (input.substr(0,input.find("="))); 
+}
+void print_vector(vector<string> vect)
 {
     for(int i = 0; i < vect.size(); i++)
     {
