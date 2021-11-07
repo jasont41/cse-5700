29f4ddf84d2655fb83c78792d2df02c81a43b7dc
Jason Eastman <jasont41@gmail.com>
2021-11-06 17:27:22 -0700

    more lab2

--- Lab2/Lex.cpp
+++ Lab2/Lex.cpp
@@ -1,13 +1,16 @@
 #include <fstream> 
 #include <iostream> 
 #include <vector> 
+#include <string> 
 using namespace std; 
 
 void vector_print(vector<string>); 
-
+int check_line(vector<string>&, vector<int>&, string, int); 
 int main()
 {
     string line; 
+    vector<string> identifier; 
+    vector<int> location; 
     ifstream fin("/Users/jasoneastman/Documents/workspace/cse-5700/Lab2/input.txt"); 
 
     vector<string> lines; 
@@ -20,12 +23,31 @@ int main()
     {
         lines.push_back(line); 
     }
-
-
+    //vector_print(lines); 
+    for(int i = 0; i < lines.size(); i++){
+        check_line(identifier, location, lines[i], i); 
+    }
 
     
 }
 
+
+
+int check_line(vector<string>& ident, vector<int>& loc, string line, int line_num)
+{
+    line_num++;  //zero based making it one based 
+    bool found_identifier = false; 
+    string sub[7] =  {"char", "short", "long", "int", "float","double","bool"}; 
+    for(int i = 0; i < 7; i++)
+    {
+        if(line.find(sub[i]) != string::npos){
+            cout << "found" << endl << i << endl; 
+            return 1;  
+        }
+    }
+
+
+}
 
 void vector_print(vector<string> vect)
 {
